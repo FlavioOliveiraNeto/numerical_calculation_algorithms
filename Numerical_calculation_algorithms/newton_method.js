44158281587fc21c33b134a26a25175de4de7df4
Flavio de Oliveira Neto <95692165+FlavioOliveiraNeto@users.noreply.github.com>
2022-03-22 21:22:24 -0300

    Add files via upload

--- Numerical_calculation_algorithms/newton_method.js
+++ Numerical_calculation_algorithms/newton_method.js
@@ -1,6 +1,6 @@
-//Algorithm made by Flavio de Oliveira Neto that uses the fixed-point method to find roots of functions
+//Algorithm made by Flavio de Oliveira Neto that uses the Newton method to find roots of functions
 //This method needs:
-//2 functions, one to find the root of it (that we'll call f(x)) and the other to calculate the next approximate 'x' (that we'll call g(x)); 
+//1 function and it derivate; 
 //an interval to find the approximate 'x' between this interval; 
 //an initial 'x'; 
 //a precision constant to compare the approximate 'x'. 
@@ -16,12 +16,19 @@ function main(initial_x, precision) {
         console.log('Iterações: ' + iteration); //print the number of the actual iteration
         console.log('X barra: ' + nxt_x); //print the actual value of 'x'
         console.log('F de X barra: ' + fx_calculate(nxt_x)); //print the actual value of f(x) function with the actual 'x'
+        console.log('Derivada de F de X barra: ' + fx_derivate_calculate(nxt_x)); //print the actual value of f(X) derivate with the actual 'x'
         console.log("---------------------------------");
 
-        nxt_x = gx_calculate(nxt_x); //switch the value of the variable nxt_x to the next value of 'x', calculate by the g(x) function
+        var condition = 'True'; //variable condition to make the protocol run until condition turns 'False'
+
+        if(Math.abs(fx_calculate(nxt_x)) <= precision) {
+            condition = 'False';
+        }
+
+        nxt_x = newton_formula(nxt_x, fx_calculate(nxt_x), fx_derivate_calculate(nxt_x)); //switch the value of the variable nxt_x to the next value of 'x', calculate by the Newton formula
 
         iteration++; //add +1 to the iteration variable 
-    }while(Math.abs(fx_calculate(nxt_x)) > precision); //do this protocol while the value of f(x) is smaller than the precision
+    }while(condition == 'True'); //do this protocol while the value of condition is True
 }
 
 //function to calculate the value of f(x) function used to compare with the precision, takes actual 'x' value as paramether
@@ -29,9 +36,14 @@ function fx_calculate(x_value) {
     return (Math.pow(Math.E, x_value) - (4 * (Math.pow(x_value, 2))));
 }
 
-//function to calculate the value of the next value of 'x' used in the f(x) function, takes actual 'x' value as paramether
-function gx_calculate(x_value) {
-    return (0.5 * (Math.pow(Math.E, (x_value / 2))));
+//function to calculate the derivate of the function using the value of 'x', takes actual 'x' value as paramether
+function fx_derivate_calculate(x_value) {
+    return (Math.pow(Math.E, x_value) - (8 * x_value));
+}
+
+//function to calculate the next value of 'x' using Newton formula
+function newton_formula(x_value, fx_value, fx_derivate_value) {
+    return ((x_value) - ((fx_value)/(fx_derivate_value)));
 }
 
 main(0.5, 0.0001); //call main function to execute the algorithm. Here I can assume the initial value of 'x' and precision value.
